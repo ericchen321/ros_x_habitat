f2e1cf816c87490f88a61738ca4e39c79d419f6a
lci-user <chenguanxiong@alumni.ubc.ca>
2021-06-09 01:45:13 -0700

    implemented env and agent node; not tested yet

--- src/classes/habitat_agent_node.py
+++ src/classes/habitat_agent_node.py
@@ -1,44 +1,248 @@
 #!/usr/bin/env python
+import argparse
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Dict,
+    List,
+)
 import rospy
-from rospy_message_converter import message_converter
 from geometry_msgs.msg import Twist
-from std_msgs.msg import String
+from ros_x_habitat.msg import PointGoalWithGPSCompass
+from sensor_msgs.msg import Image
+from std_msgs.msg import Int16
+from message_filters import TimeSynchronizer
+from cv_bridge import CvBridge, CvBridgeError
 from habitat.config import Config
 from habitat_baselines.agents.ppo_agents import PPOAgent
+from habitat.sims.habitat_simulator.actions import _DefaultHabitatSimActions
+from math import radians
+
+
+def get_default_config():
+    c = Config()
+    c.INPUT_TYPE = "blind"
+    c.MODEL_PATH = "data/checkpoints/blind.pth"
+    c.RESOLUTION = 256
+    c.HIDDEN_SIZE = 512
+    c.RANDOM_SEED = 7
+    c.PTH_GPU_ID = 0
+    c.GOAL_SENSOR_UUID = "pointgoal_with_gps_compass"
+    return c
 
 
 class HabitatAgentNode:
-    def __init__(self, agent_config: Config, enable_physics: bool = False):
+    def __init__(self, agent_config: Config, enable_physics: bool = False, control_period: float = 1.0, sensor_pub_rate: int = 10):
         self.agent = PPOAgent(agent_config)
-        self.decision = None
+        self.input_type = agent_config.INPUT_TYPE
         self.enable_physics = enable_physics
-        self.pub = rospy.Publisher("action", String, queue_size=10)
+        self.sensor_pub_rate = sensor_pub_rate
+
+        # the last action produced from the agent
+        self.action = None
+
+        # control_period defined for continuous mode
+        if self.enable_physics:
+            self.control_period = control_period
+
+        # count the number of frames received from Habitat simulator;
+        # reset to 0 every time an action is completed
+        # not applicable in discrete mode
+        if self.enable_physics:
+            self.count_frames = 0
+
+        # publish to command topics
         if self.enable_physics:
             self.pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
-        self.sub = rospy.Subscriber("observations", String, self.callback)
-
-    def callback(self, msg):
-        current_observations = message_converter.convert_ros_message_to_dictionary(msg)
-        # TODO: convert current_observations from a Python dict to habitat.core.simulator.Observations class
-        self.decision = self.agent.act(current_observations)
-        # TODO: convert decision to either a String (discrete mode) or a Twist (physics mode)
-        new_msg = String()
-        if self.enable_physics:
-            new_msg = Twist()
-            new_msg.linear.x = 0
-            new_msg.linear.y = 0
-            new_msg.linear.z = 0
-            new_msg.angular.x = 0
-            new_msg.angular.y = 0
-            new_msg.angular.z = 0
         else:
-            new_msg.data = ""
+            self.pub = rospy.Publisher("action", Int16, queue_size=10)
 
-        self.pub.publish(new_msg)
+        # subscribe to sensor topics
+        if self.input_type == "rgb" or self.input_type == "rgbd":
+            self.sub_rgb = rospy.Subscriber("rgb", Image)
+        if self.input_type == "depth" or self.input_type == "rgbd":
+            self.sub_depth = rospy.Subscriber("depth", Image)
+        self.sub_pointgoal_with_gps_compass = rospy.Subscriber("pointgoal_with_gps_compass", PointGoalWithGPSCompass)
+        
+        # filter sensor topics with time synchronizer
+        if self.input_type == "rgb":
+            self.ts = TimeSynchronizer([self.sub_rgb, self.sub_pointgoal_with_gps_compass], 10)
+            self.ts.registerCallback(self.callback_rgb)
+        elif self.input_type == "rgbd":
+            self.ts = TimeSynchronizer([self.sub_rgb, self.sub_depth, self.sub_pointgoal_with_gps_compass], 10)
+            self.ts.registerCallback(self.callback_rgbd)
+        else:
+            self.ts = TimeSynchronizer([self.sub_depth, self.sub_pointgoal_with_gps_compass], 10)
+            self.ts.registerCallback(self.callback_depth)
+
+    def msgs2obs(self, msgs: List) -> Dict[str, Any]:
+        r"""
+        Decode ROS messages into Habitat observations.
+        :param msg: Observations from simulator as a list of messages.
+        :return: Habitat observations
+        TODO: reduce hard-codeded sensor IDs
+        """
+        observations = {}
+        
+        # RGB agent
+        if self.input_type == "rgb":
+            observations["rgb"] = CvBridge().imgmsg_to_cv2(msgs[0], "rgb8")
+            observations["pointgoal_with_gps_compass"] = [msgs[1].distance_to_goal, msgs[1].angle_to_goal]
+        # RGBD agent
+        elif self.input_type == "rgbd":
+            observations["rgb"] = CvBridge().imgmsg_to_cv2(msgs[0], "rgb8")
+            observations["depth"] = CvBridge().imgmsg_to_cv2(msgs[1], "rgb8")
+            observations["pointgoal_with_gps_compass"] = [msgs[2].distance_to_goal, msgs[2].angle_to_goal]
+        # Depth agent
+        else:
+            observations["depth"] = CvBridge().imgmsg_to_cv2(msgs[0], "rgb8")
+            observations["pointgoal_with_gps_compass"] = [msgs[1].distance_to_goal, msgs[1].angle_to_goal]
+        
+        return observations
+    
+    def action2msg(self, action: Dict[str, int]):
+        r"""
+        Converts action produced by Habitat agent to a ROS message.
+        :param action: Action produced by Habitat agent.
+        :returns: A ROS message of action or velocity command.
+        """
+        action_id = action["action"]
+        msg = ...  # type: Union[Twist, Int16]
+
+        if self.enable_physics:
+            msg = Twist()
+            # Convert to Twist message in continuous mode
+            if action_id == _DefaultHabitatSimActions.STOP:
+                msg.linear.x = 0
+                msg.linear.y = 0
+                msg.linear.z = 0
+                msg.angular.x = 0
+                msg.angular.y = 0
+                msg.angular.z = 0
+            elif action_id == _DefaultHabitatSimActions.MOVE_FORWARD:
+                msg.linear.x = 0
+                msg.linear.y = 0
+                msg.linear.z = 0.25 / self.control_period
+                msg.angular.x = 0
+                msg.angular.y = 0
+                msg.angular.z = 0
+            elif action_id == _DefaultHabitatSimActions.TURN_LEFT:
+                msg.linear.x = 0
+                msg.linear.y = 0
+                msg.linear.z = 0
+                msg.angular.x = 0
+                msg.angular.y = radians(10.0) / self.control_period
+                msg.angular.z = 0
+            elif action_id == _DefaultHabitatSimActions.TURN_RIGHT:
+                msg.linear.x = 0
+                msg.linear.y = 0
+                msg.linear.z = 0
+                msg.angular.x = 0
+                msg.angular.y = radians(-10.0) / self.control_period
+                msg.angular.z = 0
+        else:
+            # Convert to Int16 message in discrete mode
+            msg = Int16()
+            msg.data = action_id
+        
+        return msg
+
+    def callback_rgb(self, img_msg_rgb):
+        r"""
+        Produce an action or velocity command periodically from RGB
+        sensor observation, and publish to respective topics.
+        :param img_msg_rgb: RGB sensor readings in ROS message format.
+        """
+        # convert current_observations from ROS to Habitat format
+        observations = self.msgs2obs([img_msg_rgb])
+
+        # produce an action/velocity once the last action has completed
+        # and publish to relevant topics
+        if self.enable_physics:
+            self.count_frames += 1
+            if self.count_frames == (self.sensor_pub_rate * self.control_period) - 1:
+                self.count_frames = 0
+                self.action = self.agent.act(observations)
+                vel_msg = self.action2msg(self.action)
+                self.pub.publish(vel_msg)
+        else:
+            self.action = self.agent.act(observations)
+            action_msg = self.action2msg(self.action)
+            self.pub.publish(action_msg)
+    
+    def callback_depth(self, img_msg_depth):
+        r"""
+        Produce an action or velocity command periodically from depth
+        sensor observation, and publish to respective topics.
+        :param img_msg_depth: Depth sensor readings in ROS message format.
+        """
+        # convert current_observations from ROS to Habitat format
+        observations = self.msgs2obs([img_msg_depth])
+
+        # produce an action/velocity once the last action has completed
+        # and publish to relevant topics
+        if self.enable_physics:
+            self.count_frames += 1
+            if self.count_frames == (self.sensor_pub_rate * self.control_period) - 1:
+                self.count_frames = 0
+                self.action = self.agent.act(observations)
+                vel_msg = self.action2msg(self.action)
+                self.pub.publish(vel_msg)
+        else:
+            self.action = self.agent.act(observations)
+            action_msg = self.action2msg(self.action)
+            self.pub.publish(action_msg)
+    
+    def callback_rgbd(self, img_msg_rgb, img_msg_depth):
+        r"""
+        Produce an action or velocity command periodically from RGBD
+        sensor observations, and publish to respective topics.
+        :param img_msg_rgb: RGB sensor readings in ROS message format.
+        :param img_msg_depth: Depth sensor readings in ROS message format.
+        """
+        # convert current_observations from ROS to Habitat format
+        observations = self.msgs2obs([img_msg_rgb, img_msg_depth])
+
+        # produce an action/velocity once the last action has completed
+        # and publish to relevant topics
+        if self.enable_physics:
+            self.count_frames += 1
+            if self.count_frames == (self.sensor_pub_rate * self.control_period) - 1:
+                self.count_frames = 0
+                self.action = self.agent.act(observations)
+                vel_msg = self.action2msg(self.action)
+                self.pub.publish(vel_msg)
+        else:
+            self.action = self.agent.act(observations)
+            action_msg = self.action2msg(self.action)
+            self.pub.publish(action_msg)
 
 
 if __name__ == "__main__":
+    # parse input arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--input-type",
+        default="blind",
+        choices=["blind", "rgb", "depth", "rgbd"],
+    )
+    parser.add_argument("--model-path", default="", type=str)
+    parser.add_argument('--enable-physics', default=False, action='store_true')
+    parser.add_argument(
+        "--control-period",
+        type=float,
+        default=1.0,
+    )
+    parser.add_argument(
+        "--sensor-pub-rate",
+        type=int,
+        default=10,
+    )
+    args = parser.parse_args()
+
     rospy.init_node("agent_node")
-    # TODO: add CLI args parsing to support launching individually
-    HabitatAgentNode(None)
+    agent_config = get_default_config()
+    agent_config.INPUT_TYPE = args.input_type
+    agent_config.MODEL_PATH = args.model_path
+    HabitatAgentNode(agent_config=agent_config, enable_physics=args.enable_physics, control_period=args.control_period, sensor_pub_rate=args.sensor_pub_rate)
     rospy.spin()
--- src/classes/habitat_env_node.py
+++ src/classes/habitat_env_node.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
-from typing import Optional
+import argparse
+from typing import (
+    TYPE_CHECKING,
+    Optional,
+)
+import numpy as np
 import rospy
 from rospy_message_converter import message_converter
 from geometry_msgs.msg import Twist
-from std_msgs.msg import String
+from ros_x_habitat.msg import PointGoalWithGPSCompass
+from sensor_msgs.msg import Image
+from std_msgs.msg import Header, Int16
+from habitat.core.simulator import Observations
+from cv_bridge import CvBridge, CvBridgeError
 from classes.habitat_eval_rlenv import HabitatEvalRLEnv
 from habitat.config.default import get_config
 
@@ -12,33 +21,134 @@ class HabitatEnvNode:
     def __init__(
         self, config_paths: Optional[str] = None, enable_physics: bool = False
     ):
+        r"""
+        Instantiates a ROS node which encapsulates a Habitat simulator.
+        The node subscribes to agent command topics, and publishes sensor
+        readings to sensor topics.
+        """
+        # initialize Habitat environment
         self.config = get_config(config_paths)
         self.enable_physics = enable_physics
         self.env = HabitatEvalRLEnv(
             config=self.config, enable_physics=self.enable_physics
         )
         self.observations = self.env.reset()
-        self.pub = rospy.Publisher("observations", String, queue_size=10)
-        self.sub = rospy.Subscriber("action", String, self.callback)
+
+        # publish to sensor topics
+        for sensor_uuid, _ in self.observations.items():
+            # we create one topic for each of RGB, Depth and GPS+Compass
+            # sensor
+            if sensor_uuid == "rgb":
+                self.pub_rgb = rospy.Publisher(sensor_uuid, Image, queue_size=10)
+            elif sensor_uuid == "depth":
+                self.pub_depth = rospy.Publisher(sensor_uuid, Image, queue_size=10)
+            elif sensor_uuid == "pointgoal_with_gps_compass":
+                self.pub_pointgoal_with_gps_compass = rospy.Publisher(sensor_uuid, PointGoalWithGPSCompass, queue_size=10)
+
+        # subscribe to command topics
         if self.enable_physics:
             self.sub = rospy.Subscriber("cmd_vel", Twist, self.callback)
-        # TODO: publish initial observations after __init__
+        else:
+            self.sub = rospy.Subscriber("action", Int16, self.callback)
 
-    def callback(self, msg):
-        agent_decision = msg.data
-        # TODO: convert agent's decision from a String or a Twist to appropriate Action class that the Env accepts
-        (self.observations, _, _, _) = self.env.step(agent_decision)
-        # TODO: convert observations to Python dictionary then to a ROS String
-        # e.g dictionary = { 'data': 'Howdy' }
-        # message = message_converter.convert_dictionary_to_ros_message('std_msgs/String', dictionary)
-        new_msg = String()
-        new_msg.data = ""
+        # publish initial observations after __init__
+        # encode observations as ROS messages
+        observations_ros = self.obs2msgs(self.observations)
+        for sensor_uuid, _ in self.observations.items():
+            # we publish to each of RGB, Depth and GPS+Compass sensor
+            if sensor_uuid == "rgb":
+                self.pub_rgb.publish(observations_ros["rgb"])
+            elif sensor_uuid == "depth":
+                self.pub_depth.publish(observations_ros["depth"])
+            elif sensor_uuid == "pointgoal_with_gps_compass":
+                self.pub_pointgoal_with_gps_compass.publish("pointgoal_with_gps_compass")
+    
+    def obs2msgs(self, observations_hab: Observations):
+        r"""
+        Converts Habitat observations to ROS observations.
 
-        self.pub.publish(new_msg)
+        :param observations_hab: Habitat observations.
+        :return: a dictionary containing RGB/depth/Pos+Orientation readings
+        in ROS Image/Pose format.
+        """
+        observations_ros = {}
+
+        # take the current sim time to later use as timestamp
+        # for all simulator readings
+        t_curr = rospy.Time.now()
+
+        for sensor_uuid, _ in self.observations.items():
+            sensor_data = self.observations[sensor_uuid]
+            # we publish to each of RGB, Depth and GPS+Compass sensor
+            if sensor_uuid == "rgb": 
+                img_msg_rgb = CvBridge().cv2_to_imgmsg(sensor_data.astype(np.uint8), encoding="rgb8")
+                img_msg_rgb.height, img_msg_rgb.width = sensor_data.shape
+                h = Header()
+                h.stamp = t_curr
+                img_msg_rgb.header = h
+                observations_ros[sensor_uuid] = img_msg_rgb
+            elif sensor_uuid == "depth":
+                img_msg_depth = CvBridge().cv2_to_imgmsg(sensor_data.astype(np.uint8), encoding="mono16")
+                img_msg_depth.height, img_msg_depth.width = sensor_data.shape
+                h = Header()
+                h.stamp = t_curr
+                img_msg_depth.header = h
+                observations_ros[sensor_uuid] = img_msg_depth
+            elif sensor_uuid == "pointgoal_with_gps_compass":
+                ptgoal_with_gps_compass_msg = PointGoalWithGPSCompass()
+                ptgoal_with_gps_compass_msg.distance_to_goal = sensor_data[0]
+                ptgoal_with_gps_compass_msg.angle_to_goal = sensor_data[1]
+                h = Header()
+                h.stamp = t_curr
+                ptgoal_with_gps_compass_msg.header = h
+                observations_ros[sensor_uuid] = ptgoal_with_gps_compass_msg
+        return observations_ros
+
+    def publish_sensor_observations(self, rate: int = 10):
+        r"""
+        Publishes simulator sensor readings indefinitely
+        at given rate.
+        """
+        r = rospy.Rate(rate)
+        
+        # publish observations at fixed rate
+        while not rospy.is_shutdown():
+            observations_ros = self.obs2msgs(self.observations)
+            for sensor_uuid, _ in self.observations.items():
+                # we publish to each of RGB, Depth and GPS+Compass sensor
+                if sensor_uuid == "rgb":
+                    self.pub_rgb.publish(observations_ros["rgb"])
+                elif sensor_uuid == "depth":
+                    self.pub_depth.publish(observations_ros["depth"])
+                elif sensor_uuid == "pointgoal_with_gps_compass":
+                    self.pub_pointgoal_with_gps_compass.publish("pointgoal_with_gps_compass")
+            r.sleep()
+
+    def callback(self, cmd_msg):
+        # unpack agent action from ROS message, and send the action
+        # to the simulator 
+        if self.enable_physics is True:
+            # TODO: invoke step_physics() or something to set velocity
+            pass 
+        else:
+            agent_action = cmd_msg.data
+            (self.observations, _, _, _) = self.env.step(agent_action)
 
 
 if __name__ == "__main__":
+    # parse input arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--task-config", type=str, default="configs/pointnav_d_orignal.yaml"
+    )
+    parser.add_argument('--enable-physics', default=False, action='store_true')
+    parser.add_argument(
+        "--sensor-pub-rate",
+        type=int,
+        default=10,
+    )
+    args = parser.parse_args()
+
     rospy.init_node("env_node")
-    # TODO: add CLI args parsing to support launching individually
-    HabitatEnvNode(None)
-    rospy.spin()
+    HabitatEnvNode(config_paths=args.task_config, enable_physics=args.enable_physics)
+    HabitatEnvNode.publish_sensor_observations(args.sensor_pub_rate)
